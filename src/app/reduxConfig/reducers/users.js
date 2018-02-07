ff7c965654ab9af331cfa754104af7832b72d1b3
erick <erickarmandogarcia@hotmail.com>
2018-02-07 17:37:15 -0500

    <users.js>Implement cases in users reducers for login

--- src/app/reduxConfig/reducers/users.js
+++ src/app/reduxConfig/reducers/users.js
@@ -1,21 +1,21 @@
-import { FETCH_USERS_REQUEST } from '../constants/constants'
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS
+} from '../constants/constants'
 
 const initialState = {
   isFecthing: false,
   error: '',
-  users: []
+  users: [],
+  success: false
 }
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_USERS_REQUEST:
-      // fake users
-      let usersList = [
-        { 'name': 'user 1', 'lastName': 'last name user1' },
-        { 'name': 'user 2', 'lastName': 'last name user2' }
-      ]
-      // update the state
-      return Object.assign({}, state, { users: usersList })
+    case LOGIN_REQUEST:
+      return Object.assign({}, state, {isFecthing: true, error: ''})
+    case LOGIN_SUCCESS:
+      return Object.assign({}, state, {isFecthing: false, success: true})
 
     default:
       return state
