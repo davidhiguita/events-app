d88d400fd7c3c922a86eed7e8265dbad11f2ac4d
erick <erickarmandogarcia@hotmail.com>
2018-02-12 11:06:44 -0500

    Refactor connection with redux in index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // import react dependencies
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 
 // Redux stuff
 import { combineReducers, createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import createHistory from 'history/createHashHistory'
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import {createBrowserHistory} from 'history'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
 // main app component
 import Login from 'components/Login/Login'
 import SignUp from 'components/SignUp/SignUp'
@@ -29,7 +29,7 @@ const mainReducer = combineReducers({
   routing: routerReducer
 })
 
-const history = createHistory()
+const history = createBrowserHistory()
 
 const store = createStore(
   mainReducer,
@@ -42,19 +42,26 @@ const store = createStore(
     window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 )
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Route path='/login' component={Login} />
+        <Route path='/signUp' component={SignUp} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/event-detail/:id' component={DashboardEventDetail} />
+      </div>
+    )
+  }
+}
+
 const MyApp = () => {
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Router>
-          <div>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/signUp' component={SignUp} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/event-detail/:id' component={DashboardEventDetail} />
-          </div>
-        </Router>
-      </ConnectedRouter>
+      <Router history={history}>
+        <App />
+      </Router>
     </Provider>
   )
 }
