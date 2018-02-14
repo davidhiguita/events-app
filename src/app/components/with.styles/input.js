fc244f0b677098744a064fbd1bfc78696b0934d2
erick <erickarmandogarcia@hotmail.com>
2018-02-14 14:52:56 -0500

    Add some styles for input boxes, to reuse in all pages with input

---
+++ src/app/components/with.styles/input.js
@@ -0,0 +1,15 @@
+const inputStyles = {
+  rootLabel: {
+    color: '#D8D8D8'
+  },
+  inkbar: {
+    '&:after': {
+      backgroundColor: '#323C46'
+    },
+    '&:before': {
+      backgroundColor: '#DAE1E7'
+    }
+  }
+}
+
+export default inputStyles
