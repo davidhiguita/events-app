bb736b0b38977bc58a93a2c457ebc8d005a70f7d
erick <erickarmandogarcia@hotmail.com>
2018-02-14 20:37:23 -0500

    Add close icon in new event page

--- src/app/components/Dashboard/DashboardEventNew/dashboard.event.new.jsx
+++ src/app/components/Dashboard/DashboardEventNew/dashboard.event.new.jsx
@@ -7,22 +7,37 @@ import dashboardActions from 'reduxConfig/actions/dashboard'
 import loginActions from 'reduxConfig/actions/login'
 // material ui stuff
 import Grid from 'material-ui/Grid'
+import CloseIcon from 'material-ui-icons/close'
 // components
 import DashboardEventNewForm from 'components/Dashboard/DashboardEventNew/DashboardEventNewForm/dashboard.event.new.form'
 
 class DashboardEventNew extends Component {
+  constructor (props) {
+    super(props)
+    this.redirectToDashboardPage = this.redirectToDashboardPage.bind(this)
+  }
+
+  redirectToDashboardPage () {
+    const { history } = this.props
+    history.push(`/dashboard`)
+  }
+
   buildContent () {
-    const { events: { tokenExpired }, loginFailure } = this.props
+    const { events: { tokenExpired }, loginFailure, history } = this.props
     // redirect to login page if the token has expired
     if (tokenExpired) {
       // dispatch this to show up the error in login page
       loginFailure('The session has expired, please login')
-      return <Redirect to={{pathname: `/login`}} />
+      history.push(`/dashboard`)
     }
 
     return (
       <Grid container className='dasboard-event-new' spacing={24}>
         <Grid item md={12} sm={12} xs={12}>
+          <div className='dasboard-event-new__top-bar'>
+            <span className='dasboard-event-new__top-bar-icon'><CloseIcon onClick={this.redirectToDashboardPage} /></span>
+            <span className='dasboard-event-new__top-bar-text'>Close</span>
+          </div>
           <div className='dasboard-event-new__container'>
             <DashboardEventNewForm {...this.props} />
           </div>
